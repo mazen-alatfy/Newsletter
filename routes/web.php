a65d89d433eb73093e6a4cc991338b7c8c7a10ce
Khaled AbdElRahim <khaled710017@gmail.com>
2025-04-19 03:47:53 +0200

    order posts by latest

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.posts.index', ['posts' => Post::all()]);
+    return view('pages.posts.index', ['posts' => Post::latest()->get()]);
 });
 
 Route::get('/posts/{post}', function (Post $post) {
